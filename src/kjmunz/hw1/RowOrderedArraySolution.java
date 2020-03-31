de3ce574d615f90f21f9ca6a6d23b5f3c30db757
K Munz <kijamu@gmail.com>
2020-03-31 11:12:08 -0400

    Initialize Homework 1

---
+++ src/kjmunz/hw1/RowOrderedArraySolution.java
@@ -0,0 +1,62 @@
+package kjmunz.hw1;
+
+import algs.hw1.arraysearch.RowOrderedArraySearch;
+
+/**
+ * Copy this class into your package, which must have USERID has its root.
+ */
+public class RowOrderedArraySolution extends RowOrderedArraySearch {
+
+	/** Construct problem solution for given array. Do not modify this method. */
+	public RowOrderedArraySolution(int[][] a) {
+		super(a);
+	}
+	
+	/** 
+	 * For this homework assignment, you need to complete the implementation of this
+	 * method.
+	 */
+	@Override
+	public int[] locate(int target) {
+		// FIX ME: complete this method. Currently this causes an exception to be thrown
+		int n = this.length();
+		int low = 0;
+		int high = n-1;
+
+		while (low <= high) {
+			int mid = (low+high)/2;
+
+			int rc = inspect(mid, 0) - target;
+			if (rc < 0) {
+				low = mid+1;
+			} else if (rc > 0) {
+				high = mid-1;
+			} else {
+				return new int[] {mid,0};
+			}
+		}
+
+		int r = high;
+		low = 0;
+		high = r;
+		while (low <= high) {
+			int mid = (low+high)/2;
+
+			int rc = inspect(r, mid) - target;
+			if (rc < 0) {
+				low = mid+1;
+			} else if (rc > 0) {
+				high = mid-1;
+			} else {
+				return new int[] {mid,0};
+			}
+		}
+		return null; // not found
+	}
+	
+	/** Be sure that you call your class constructor. Do not modify this method. */ 
+	public static void main (String args[]) {
+		int[][] ar = RowOrderedArraySearch.create(13);
+		new RowOrderedArraySolution(ar).trial();
+	}
+}
